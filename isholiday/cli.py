b8232cab7ee0771457841745b322b001d96a1ee8
Tim Birkett <tim.birkett@itv.com>
2018-07-20 15:19:23 +0100

    Initial Commit

---
+++ isholiday/cli.py
@@ -0,0 +1,22 @@
+import sys
+import holidays
+import click
+import logging
+
+from datetime import date
+
+
+@click.command()
+@click.option('--country', '-c', default='UK',
+              help='Country you are running in.')
+def main(country):
+    """Is it a bank holiday?"""
+
+    logging.basicConfig(format='[%(levelname)s] %(message)s')
+    try:
+        my_holidays = getattr(holidays, country)
+    except AttributeError:
+        logging.error("Country: %s not found." % country)
+        sys.exit(127)
+
+    sys.exit(date.today() not in my_holidays())
---
+++ tests/test_cli.py
@@ -0,0 +1,21 @@
+import pytest
+from click.testing import CliRunner
+from isholiday import cli
+
+
+@pytest.fixture
+def runner():
+    return CliRunner()
+
+
+def test_cli(runner, caplog):
+    result = runner.invoke(cli.main)
+    assert result.exit_code in (0, 1)
+    assert caplog.text == ''
+
+
+def test_cli_with_unknown_country(runner, caplog):
+    result = runner.invoke(cli.main, ['--country', 'UNKNOWNCOUNTRY'])
+    assert result.exception
+    assert result.exit_code == 127
+    assert 'Country: UNKNOWNCOUNTRY not found.' in caplog.text
