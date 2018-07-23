09c5152038a26ba052dc0d3816c65d86a1cc1303
Tim Birkett <tim.birkett@itv.com>
2018-07-23 16:11:36 +0100

    Make countries a little more fool proof

--- publicholiday/cli.py
+++ publicholiday/cli.py
@@ -13,18 +13,20 @@ def main(country):
     """Is it a public holiday?"""
 
     logging.basicConfig(format='[%(levelname)s] %(message)s')
-    try:
-        my_holidays = getattr(holidays, country)
-    except AttributeError:
-        try:
-            # Account for full country names entered in lowercase
-            my_holidays = getattr(holidays, country.title())
-        except AttributeError:
-            try:
-                # Account for country code entered lowercase
-                my_holidays = getattr(holidays, country.upper())
-            except AttributeError:
-                logging.error("Country: %s not found." % country)
-                sys.exit(127)
 
-    sys.exit(date.today() not in my_holidays())
+    country_methods = holidays.__dict__.keys()
+    country_name = next((x for x in country_methods if
+                         country.lower() == x.lower()), '')
+
+    try:
+        country_holidays = getattr(holidays, country_name)
+        try:
+            isinstance(country_holidays(), holidays.HolidayBase)
+        except TypeError:
+            raise AttributeError
+
+    except AttributeError:
+        logging.error("Country: %s not found." % country)
+        sys.exit(127)
+
+    sys.exit(date.today() not in country_holidays())
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -28,6 +28,13 @@ def test_cli_with_strangecase_country(runner, caplog):
     assert caplog.text == ''
 
 
+def test_cli_with_non_country(runner, caplog):
+    result = runner.invoke(cli.main, ['--country', 'MONDAY'])
+    assert result.exception
+    assert result.exit_code == 127
+    assert 'Country: MONDAY not found.' in caplog.text
+
+
 def test_cli_with_unknown_country(runner, caplog):
     result = runner.invoke(cli.main, ['--country', 'UNKNOWNCOUNTRY'])
     assert result.exception
